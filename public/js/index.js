e77c5e4264332342296eecd0e0c621a262445c30
SantiiMorales <santimoralessgr@gmail.com>
2024-06-20 02:53:18 -0400

    vercel

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,6 @@
 window.sr = ScrollReveal();
 sr.reveal('.Titulo', {
-    duration: 1500,
+    duration: 1800,
     origin: 'bottom',
     distance:'-80px'
 })
@@ -10,19 +10,19 @@ sr.reveal('.titulo-seccion', {
     distance:'-400px'
 })
 sr.reveal('.seccion-noticias', {
-    duration: 1500,
+    duration: 2000,
     origin: 'top',
     distance:'-400px',
     delay: 1000
 })
 sr.reveal('.contenedor-contacto', {
-    duration: 1500,
+    duration: 2500,
     origin: 'top',
     distance:'-80px',
     delay: 800
 })
 sr.reveal('.seccion-planetas', {
-  duration: 1000,
+  duration: 2000,
   origin: 'left',
   distance:'-400px',
   delay: 1000
